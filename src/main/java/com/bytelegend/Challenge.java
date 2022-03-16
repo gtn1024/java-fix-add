89c45d5b2b710f7fd7b89b73b03c5d8666b1d53c
Lh549 <1606953936@qq.com>
2022-03-16 07:48:15 +0000

    Update at 2022-03-16T07:48:15.899017Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -2,10 +2,10 @@ package com.bytelegend;
 
 public class Challenge {
     public static int add(int a, int b) {
-        return a - b;
+        return a + b;
     }
 
     public static void main(String[] args) {
-        System.out.println("1-1=" + add(1, 1));
+        System.out.println("1+1=" + add(1, 1));
     }
 }
