9b8e2a1a4652d1f650e254cc99029642f224326f
DeerSayHello <deerzfhu@126.com>
2023-03-23 10:01:11 +0000

    Update at 2023-03-23T10:01:11.163094Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -6,6 +6,6 @@ public class Challenge {
     }
 
     public static void main(String[] args) {
-        System.out.println("1+1=" + add(1, 1));
+        System.out.println("1-1=" + add(1, 1));
     }
 }
